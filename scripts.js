de389818523fd0d8c8a3f809eefcdab3617312c3
alisaduncan <7586237+alisaduncan@users.noreply.github.com>
2018-05-20 10:32:29 -0500

    starting point for worksheets

---
+++ scripts.js
@@ -0,0 +1,31 @@
+const addDevSkill = function (skill) {
+	let listItem = document.createElement('li');
+	listItem.innerHTML = skill;
+	document.getElementById("coding-section").appendChild(listItem);
+}
+
+
+
+const <verb> = function (item) {
+	return 'I\'d like a ' + item + ', please!';
+}
+const <noun> = 'cocktail';
+
+
+
+const clickHandler = function (text) {
+
+
+}
+
+
+// Declare brainSkills array below this line
+
+
+
+document.addEventListener("DOMContentLoaded", function (event) {
+
+
+	document.getElementById('cocktail-section').innerHTML = <verb>(<noun>);
+
+});
